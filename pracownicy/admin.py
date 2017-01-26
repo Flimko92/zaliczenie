b078b61554fc5b1ecd956496f6b4ea9d21d063d5
Korneliusz <kornellukasiak@gmail.com>
2017-01-26 23:05:24 +0100

    Dane osobowe pracowników + wyszukiwarka

--- pracownicy/admin.py
+++ pracownicy/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Worker
+
+class WorkerAdmin(admin.ModelAdmin):
+ search_fields = ['Name','Surname']
+ order_by = ['Surname']
 # Register your models here.
+
+admin.site.register(Worker, WorkerAdmin)
+#admin.site.register([Worker])
+
+
+
+
+
--- pracownicy/models.py
+++ pracownicy/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
 
-from django.db import models
 
+from django.db import models
+#from django.contrib.auth.models import AbstractUser
+
+class Worker(models.Model):
+	Name = models.CharField(max_length=20)
+	Surname = models.CharField(max_length=30)
+	PESEL = models.CharField(max_length=11)
+	def __str__(self):
+		return "{Name} {Surname}".format(Name=self.Name,Surname=self.Surname)
 # Create your models here.
