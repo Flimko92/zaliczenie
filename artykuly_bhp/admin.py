18f3f1048c3cb7502fe83cb111c95e416bb99f78
Hubert <kolekcjoner205@interia.pl>
2017-02-01 10:51:12 +0100

    Informacje o produktach i przydziałach - tablice danych(klasy)

--- artykuly_bhp/admin.py
+++ artykuly_bhp/admin.py
@@ -1,3 +1,13 @@
+# coding: utf-8
 from django.contrib import admin
+from models import przydzial, produkty
 
-# Register your models here.
+class ProduktyAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(produkty, ProduktyAdmin)
+
+class PrzydzialAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(przydzial, PrzydzialAdmin)
--- artykuly_bhp/models.py
+++ artykuly_bhp/models.py
@@ -1,5 +1,23 @@
+# coding: utf-8
 from __future__ import unicode_literals
 
 from django.db import models
+from pracownicy.models import Worker
 
-# Create your models here.
+class produkty(models.Model):
+
+    name = models.CharField(max_length=40)
+    count = models.PositiveIntegerField()
+    exp_date = models.DateField()
+
+    def __unicode__(self):
+        return u"%s / %s" % (self.name, self.exp_date)
+
+class przydzial(models.Model):
+
+    who = models.ForeignKey(Worker)
+    what = models.ManyToManyField(produkty)
+    when = models.DateField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self):
+        return u"%s" % (self.when)
