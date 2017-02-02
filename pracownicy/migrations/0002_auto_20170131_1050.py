115401ab644c9ef40726beebf43f3daffdbb8aa0
Sebastian Puchala <sebastianpuchala@gmail.com>
2017-02-02 12:14:50 +0100

    zmiana migracji II

--- pracownicy/migrations/0002_auto_20170131_1050.py
+++ pracownicy/migrations/0002_auto_20170131_1050.py
@@ -16,13 +16,16 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Course',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('date_of_instruction', models.DateField()),
-                ('next_instruction', models.DateField()),
-                ('instruction', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='szkolenia.instruction')),
-            ],
+
+        migrations.AddField(
+            model_name='worker',
+            name='user',
+            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='worker',
+            name='course',
+            field=models.ManyToManyField(to='pracownicy.Course'),
         ),
     ]
